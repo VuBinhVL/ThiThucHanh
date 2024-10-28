db79974c584c779ef225892f5bc07c58f752e6ea
Hồ Tiến Vũ Bình <146900914+VuBinhVL@users.noreply.github.com>
2024-10-28 15:22:29 +0700

    Update scriptDataMau.sql
    .

--- scriptDataMau.sql
+++ scriptDataMau.sql
@@ -6,8 +6,8 @@ INSERT INTO Roles (RoleName) VALUES
 
 
 INSERT INTO AppUsers (Name,UserName, Password, Email, PhoneNumber, RoleId,IsLock) VALUES
-(N'Dũng Nè','admin', '123456', NULL, NULL, 1,0),
-(N'Nguyễn Văn A','khachhang', '123456', NULL, NULL, 2,0);
+(N'Vũ Bình','admin', '123456', NULL, NULL, 1,0),
+(N'Nguyễn Văn Á','khachhang', '123456', NULL, NULL, 2,0);
 
 
 INSERT INTO Categories(Name) VALUES
